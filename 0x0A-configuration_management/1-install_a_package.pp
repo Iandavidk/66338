924c10681114563bd0af1ff70b5fc078761b18df
Iandavidk <eandavid6@gmail.com>
2022-12-12 03:44:51 +0000

    Modified install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint
-package{'puppet-lint':
-ensure   => '2.5.0',
-provider => 'gem'
+# Install flask using pip3
+exec { "install_flask":
+	command => "pip3 install Flask==2.1.0",
+	path => ["/usr/bin/", "/usr/local/bin"],
 }
